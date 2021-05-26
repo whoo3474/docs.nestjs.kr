f1588b2e9d09f715735a3d9795429837ca2f43bb
Jake Kwak <jakekwak@gmail.com>
2021-05-25 22:37:38 -0500

    타이틀및 메타태그 변경

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,2 +1,2 @@
-export const HOMEPAGE_TITLE = 'Documentation';
-export const TITLE_SUFFIX = ' | NestJS - A progressive Node.js framework';
+export const HOMEPAGE_TITLE = '메인';
+export const TITLE_SUFFIX = ' | 네스트JS 한국어 매뉴얼';
